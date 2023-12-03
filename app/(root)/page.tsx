5c343fda8b331a0634a88998786a8284c3ea3d0f
Shubham <shubhamsarkar9654@gmail.com>
2023-12-03 14:16:49 +0530

    - prisma init - shadcn init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,32 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from "react";
 
 import { Modal } from "@/components/ui/modal";
+import { userStoreModal } from "@/hooks/use-store-modal";
 
 export default function Home() {
+  const [isMounted, setIsMounted] = useState(false);
+  const onOpen = userStoreModal((state) => state.onOpen)
+  const isOpen = userStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if(!isOpen) {
+      onOpen();
+    }
+  }, [onOpen, isOpen]);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="text-3xl">
-      {/* <UserButton afterSignOutUrl="/"/> */}
-      <Modal title="Test" description="description" isOpen onClose={() => {}}>
-        children
-      </Modal>
+      Root Page dfdfdf
     </div>
   );
 }
