5c5fca20f09145bc869980cad289a51c7edc16ad
Shubham <shubhamsarkar9654@gmail.com>
2023-12-11 00:40:15 +0530

    - integrated category flow - api, route, components etc...

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 
 export const useOrigin = () => {
   const [mounted, setMounted] = useState(false);
-  const origin = typeof window !== undefined && window?.location?.origin ? window.location.origin : '0';
+  const origin = window && typeof window !== undefined && window?.location?.origin ? window.location.origin : '0';
 
   useEffect(() => {
     setMounted(true);
