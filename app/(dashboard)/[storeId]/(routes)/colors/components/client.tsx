0de99de2f7faa7d22c43cae00b2880658051ecb8
Shubham <shubhamsarkar9654@gmail.com>
2023-12-12 20:55:08 +0530

    - colors created

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+import { ColorColumn, columns } from "./column";
+
+interface ColorClientProps {
+  data: ColorColumn[]
+}
+
+export const ColorClient: React.FC<ColorClientProps> = ({
+  data
+}) => {
+  const router = useRouter()
+  const params = useParams()
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 w-4 h-4 " />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading
+        title="Api"
+        description="Api calls for Billboard"
+      />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  );
+};
